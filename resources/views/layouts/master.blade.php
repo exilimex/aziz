d696a9ae530a4ad0bc71518a5691c534c64193cf
Exilimex <m.swaji@gmail.com>
2017-10-19 08:35:29 +0300

    First commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <title>Aziz</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+@include('layouts.nav')
+
+
+@yield('content')
+
+
+
+@include('layouts.footer')
+
+</body>
+
